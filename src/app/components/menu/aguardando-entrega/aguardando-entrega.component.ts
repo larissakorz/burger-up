38b251321adc0b0e409a30889b16434c1429a5a6
Larissa Korz <lariskorz@gmail.com>
2023-08-31 18:58:02 -0300

    status dos pedidos ok

--- src/app/components/menu/aguardando-entrega/aguardando-entrega.component.ts
+++ src/app/components/menu/aguardando-entrega/aguardando-entrega.component.ts
@@ -14,15 +14,24 @@ export class AguardandoentregaComponent {
   ) { }
 
   ngOnInit() {
-    this.loadOrders();
+    this.listOrders();
   }
 
-  private loadOrders() {
+  private listOrders() {
     this.orderService.getOrders().subscribe(data => {
+      data = data.filter(pedido => pedido.status === "pending")
       data.map(pedido => pedido.currentStatus = pedido.status)
       console.log(data);
       this.orders = data;
       console.log(this.orders);
     });
+  }
+
+  cancelOrder(order: any){
+    order.status = 'canceled'
+    this.orderService.editOrder(order).subscribe(data => {
+      this.orders = []
+      this.listOrders()
+    })
   }
 }
